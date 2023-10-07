f210cd953e8b1196344ec5c56f1fd9a6f922b763
Rath-san <rim.kozlowski@gmail.com>
2023-10-07 09:49:47 +0200

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Madejski Media";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_OWNER = "Mikołaj Madejski";
