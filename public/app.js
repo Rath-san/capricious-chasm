228d27c4a2daf31262a33c7d196579bfb13977af
Rath-san <rim.kozlowski@gmail.com>
2024-02-04 16:49:37 +0100

    Button animations

--- public/app.js
+++ public/app.js
@@ -1,39 +1,7 @@
 import barba from "@barba/core";
 import gsap from "gsap";
 
-// const handleCursor = () => {
-//   console.log("handle cursor");
-//   const cursor = document.querySelector(".cursor");
-
-//   let cursorInitialized = false;
-
-//   document.addEventListener("mousemove", (e) => {
-//     console.log(e);
-//     if (!cursorInitialized) {
-//       cursor?.classList.add("active");
-//       cursorInitialized = true;
-//     }
-
-//     cursor?.setAttribute(
-//       "style",
-//       "top: " + (e.pageY - 20) + "px; left: " + (e.pageX - 20) + "px;"
-//     );
-//   });
-// };
-
-// handleCursor();
-
-// barba.hooks.before(() => {
-//   barba.wrapper.classList.add("is-animating");
-// });
-// barba.hooks.after(() => {
-//   barba.wrapper.classList.remove("is-animating");
-// });
-
-// console.log(barba);
-
 export default () => {
-    // console.log('init');
   barba.init({
     transitions: [
       {
